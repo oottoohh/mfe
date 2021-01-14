c0e95cec55e8ecdbd9c668ed590f4fb9b647d5da
Mr.oottoohh <ottoh.hidayatullah@ai.astra.co.id>
2021-01-14 09:12:30 +0700

    Update App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi, Ottoh !</h1>
+      <h1>Hi, Ottoh Hidayatullah!</h1>
       <hr />
       <MarketingApp />
     </div>
