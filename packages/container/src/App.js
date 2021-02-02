f72a4610f974e8d1d5f048267ef7be6d33f428a3
Mr.oottoohh <ottoh.hidayatullah@ai.astra.co.id>
2021-02-02 10:55:23 +0700

    fixing style production
    fixing style production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi!</h1>
-      <p>Invalidation Automate</p>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
